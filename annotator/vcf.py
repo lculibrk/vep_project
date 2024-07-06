7073c487bc1136e2f0e6c3264d8afec4c4d0cec5
lculibrk <luculibrk@gmail.com>
2024-07-05 23:36:09 -0400

    Implemented reading VCFs and enforcing format compliance

--- annotator/vcf.py
+++ annotator/vcf.py
@@ -1,3 +1,23 @@
-def parse_vcf(vcf_path):
+import annotator.exceptions
+
+def read_vcf(vcf_path) -> list:
+    """
+    Read a VCF specified by the path to a list and strip the header. Enforce format compliance. 
+    """
+    ## Read the VCF, skipping header lines. Strip whitespace and split by tab.
+    with open(vcf_path, "r") as f:
+        vcf = [l for l in f.readlines() if not l.startswith("##")]
+    vcf = [l.strip().split("\t") for l in vcf]
+
+    ## Throw a useful error if VCF is completely empty with no header line
+    if len(vcf) == 0:
+        raise ValueError(f"File {vcf_path} has no VCF header line (beginning with #CHROM)")
     
+    ## Enforce that all the VCF ver.4 fixed fields are present in line 1
+    vcf_fields = ["#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT"]
+    for field in vcf_fields:
+        if field not in vcf[0]:
+            raise ValueError(f"Field {field} not found in the VCF! Are you sure this is a format compliant VCF?")
+    ## If the file is format-compliant, but empty, it should run without errors. So we won't check for length at this stage.
     return(vcf)
+
---
+++ tests/vcf.py
@@ -0,0 +1,21 @@
+import os
+import unittest
+
+from annotator import vcf
+
+class test_load_vcf(unittest.TestCase):
+    def test_load_platypus_vcf(self):
+        """
+        Test loading a platypus-formatted VCF
+        """
+        vcf_path = os.path.join(os.path.dirname(__file__), "data", "platypus.vcf")
+        vcf_file = vcf.read_vcf(vcf_path)
+        self.assertEqual(len(vcf_file), 14)
+
+    def test_load_empty_vcf(self):
+        """
+        Test loading a VCF with no variants
+        """
+        vcf_path = os.path.join(os.path.dirname(__file__), "data", "empty.vcf")
+        vcf_file = vcf.read_vcf(vcf_path)
+        self.assertEqual(len(vcf_file), 1)
