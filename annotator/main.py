ec104cfb2c6dc25a6e916ee962d6abc3b7aa277f
lculibrk <luculibrk@gmail.com>
2024-07-06 23:19:41 -0400

    Implement annotation of variants, combining variants with their annotations, and writing output to disk

--- annotator/main.py
+++ annotator/main.py
@@ -1,7 +1,9 @@
+import os
+
 import annotator.vcf
 import annotator.vep
 
-def annotator(vcf_path, total_cov_field, var_cov_field, sample_name = None):
+def run_annotator(vcf_path, output, total_cov_field, var_cov_field, sample_name = None, chunk_size = 200):
     """
     Reads and parses the input VCF, makes an API call to Ensembl VEP, annotates variants and returns the resulting JSON
     
@@ -11,6 +13,8 @@ def annotator(vcf_path, total_cov_field, var_cov_field, sample_name = None):
         total_cov_field (str): the name of the FORMAT field that contains TOTAL coverage.
         var_cov_field (str):   the name of the FORMAT field that contains VARIANT (non-reference) coverage.
         sample_name (str):     the name of the sample to be processed in the VCF. If not specified, the program will take the first column after FORMAT. Default: None
+        by_gene (boolean):     whether to query for impact by transcript (default, False) or the highest impact per gene (True). Default: False
+        chunk_size (int):      the maximum number of variants to submit per VEP API call. Default: 200
 
     Returns:
         a dictionary 
@@ -20,7 +24,17 @@ def annotator(vcf_path, total_cov_field, var_cov_field, sample_name = None):
 
     ## Call the parsing function
     vcf_lines = annotator.vcf.parse_vcf(vcf, total_cov_field, var_cov_field, sample_name = sample_name)
-    
-    ## Annotate variants
-    annotations = annotator.vep.annotate_variants(vcf_lines).json()
-    return(annotations)
+
+    ## Annotate the VCF lines using the received annotations
+    out_list = annotator.vep.annotate_variants(vcf_lines, chunk_size = chunk_size)
+
+    ## Create output path if it does not exist
+    os.makedirs(os.path.dirname(output), exist_ok = True)
+
+    ## Write the tsv to output
+    with open(output, "w") as f:
+        f.write("CHROM\tPOS\tREF\tALT\tFILTER\tN_VARIANT_READS\tTOTAL_READS\tVARIANT_READ_FRACTION\tVARIANT_TYPE\tGENE_ID\tGENE_SYMBOL\tTRANSCRIPT_ID\tIMPACT\tCONSEQUENCE_TERMS\tHGVSP\tDBSNP_ID\tCOSMIC_ID\tPOP_AF\n")
+        for line in out_list:
+            f.write("%s\n" % "\t".join(line))
+
+    return(out_list)
--- tests/vep.py
+++ tests/vep.py
@@ -1,29 +1,30 @@
 import os
 import unittest
 
-from annotator import vcf, vep
+from annotator import vcf, vep, main
 
-class test_annotate_vcf(unittest.TestCase):
-    def test_annotate_platypus_vcf(self):
+class test_get_variant_annotations(unittest.TestCase):
+    def test_get_variant_annotations_platypus(self):
         """
         Test annotating a platypus VCF
         """
         vcf_path = os.path.join(os.path.dirname(__file__), "data", "platypus.vcf")
         vcf_file = vcf.read_vcf(vcf_path)
         vcf_parsed = vcf.parse_vcf(vcf_file, "NR", "NV")
-        annotations = vep.annotate_variants(vcf_parsed)
+        annotations = vep.get_variant_annotations(vcf_parsed)
         self.assertEqual(len(vcf_parsed), len(annotations))
 
-    def test_annotate_empty_vcf(self):
+    def test_get_variant_annotations_empty_vcf(self):
         """
         Test annotating a VCF with no variants
         """
         vcf_path = os.path.join(os.path.dirname(__file__), "data", "empty.vcf")
         vcf_file = vcf.read_vcf(vcf_path)
         vcf_parsed = vcf.parse_vcf(vcf_file, "NR", "NV")
-        annotations = vep.annotate_variants(vcf_parsed)
+        annotations = vep.get_variant_annotations(vcf_parsed)
         self.assertEqual(len(annotations), 0)
     
+class test_chunked_annotations(unittest.TestCase):
     def test_chunked_vep_size_1(self):
         """
         Test submitting 2 variants, one at a time to the VEP API
@@ -31,7 +32,7 @@ class test_annotate_vcf(unittest.TestCase):
         vcf_path = os.path.join(os.path.dirname(__file__), "data", "platypus.vcf")
         vcf_file = vcf.read_vcf(vcf_path)
         vcf_parsed = vcf.parse_vcf(vcf_file, "NR", "NV")
-        annotations = vep.annotate_chunks(vcf_parsed[0:2], 1)
+        annotations = vep.get_chunked_annotations(vcf_parsed[0:2], 1)
         self.assertEqual(len(annotations), 2)
     
     def test_chunked_vep_same_size(self):
@@ -41,7 +42,7 @@ class test_annotate_vcf(unittest.TestCase):
         vcf_path = os.path.join(os.path.dirname(__file__), "data", "platypus.vcf")
         vcf_file = vcf.read_vcf(vcf_path)
         vcf_parsed = vcf.parse_vcf(vcf_file, "NR", "NV")
-        annotations = vep.annotate_chunks(vcf_parsed[0:2], 2)
+        annotations = vep.get_chunked_annotations(vcf_parsed[0:2], 2)
         self.assertEqual(len(annotations), 2)
 
     def test_chunked_vep_gr_size(self):
@@ -51,5 +52,91 @@ class test_annotate_vcf(unittest.TestCase):
         vcf_path = os.path.join(os.path.dirname(__file__), "data", "platypus.vcf")
         vcf_file = vcf.read_vcf(vcf_path)
         vcf_parsed = vcf.parse_vcf(vcf_file, "NR", "NV")
-        annotations = vep.annotate_chunks(vcf_parsed[0:2], 3)
+        annotations = vep.get_chunked_annotations(vcf_parsed[0:2], 3)
         self.assertEqual(len(annotations), 2)
+
+    def test_chunked_vep_emptydata(self):
+        """
+        Test submitting empty data to get_chunked_annotations
+        """
+        vcf_path = os.path.join(os.path.dirname(__file__), "data", "empty.vcf")
+        vcf_file = vcf.read_vcf(vcf_path)
+        vcf_parsed = vcf.parse_vcf(vcf_file, "NR", "NV")
+        annotations = vep.get_chunked_annotations(vcf_parsed)
+        self.assertEqual(len(annotations), 0)
+
+class test_merge_variant_annotation(unittest.TestCase):
+    def test_intergenic_variant(self):
+        """
+        Test annotation of a variant that is intergenic. 
+        """
+        variant_list = [["9", "82929050", "A", "T", "PASS", 100, 150]]
+        expected_output = [['9', '82929050', 'A', 'T', 'PASS', 100, 150, '0.66667', 'SNV', 'NA', 'NA', 'NA', 'MODIFIER', 'NA', 'intergenic_variant', 'NA', 'NA', 'NA']]
+        annotations = vep.annotate_variants(variant_list)
+        self.assertEqual(len(annotations), 1)
+        self.assertEqual(annotations, expected_output)
+
+
+    def test_mixed_genic_intergenic_variants(self):
+        """
+        Test annotation of a variant that is intergenic, followed by a variant that is genic but unannotated.
+        """
+        variant_list = [["9", "82929050", "A", "T", "PASS", 100, 150],
+                        ["9", "83004550", "T", "G", "PASS", 58, 92]]
+        expected_output = [['9', '82929050', 'A', 'T', 'PASS', 100, 150, '0.66667', 'SNV', 'NA', 'NA', 'NA', 'MODIFIER', 'NA', 'intergenic_variant', 'NA', 'NA', 'NA'], 
+                           ['9', '83004550', 'T', 'G', 'PASS', 58, 92, '0.63043', 'SNV', 'ENSG00000165105', 'RASEF', 'ENST00000376447', 'LOW', 'ENSP00000365630.3:p.Arg384=', 'synonymous_variant', 'NA', 'NA', 'NA']]
+        annotations = vep.annotate_variants(variant_list)
+        self.assertEqual(annotations, expected_output)
+
+    def test_known_pathogenic_variants(self):
+        """
+        Test some known pathogenic cancer variants: (KRAS G12C, 12:25245351:G>T), (BRAF V600E, 7:140753336:A>T)
+        Ensure that they return COSMIC IDs so they aren't missed in a real analysis
+        """
+        variant_list = [["12", "25245351", "G", "T", "PASS", 100, 150],
+                        ["7", "140753336", "A", "T", "PASS", 58, 92]]
+        annotations = vep.annotate_variants(variant_list)
+        cosmic_ids = [line[16] for line in annotations]
+        self.assertTrue(all([id.startswith("COS") for id in cosmic_ids]))
+
+        
+
+
+
+class test_annotate_variants(unittest.TestCase):
+    def test_line_column_counts(self):
+        """
+        Test that parse_variants returns a constant number of columns
+        """
+        vcf_path = os.path.join(os.path.dirname(__file__), "data", "platypus.vcf")
+        vcf_file = vcf.read_vcf(vcf_path)
+        vcf_parsed = vcf.parse_vcf(vcf_file, "NR", "NV")
+        out_list = vep.annotate_variants(vcf_parsed)
+        it = iter(out_list)
+        the_len = len(next(it))
+        self.assertTrue(all(len(l) == the_len for l in it))
+
+    def test_full_annotation_empty(self):
+        """
+        Test that an empty input VCF results in an empty annotated mutation table
+        """
+        vcf_path = os.path.join(os.path.dirname(__file__), "data", "empty.vcf")
+        vcf_file = vcf.read_vcf(vcf_path)
+        vcf_parsed = vcf.parse_vcf(vcf_file, "NR", "NV")
+        annotations = vep.annotate_variants(vcf_parsed)
+        self.assertEqual(len(annotations), 0)
+
+    def test_by_gene_arg(self):
+        """
+        Test that the by_gene argument to annotate_variants correctly results in one gene per annotation
+        """
+        vcf_path = os.path.join(os.path.dirname(__file__), "data", "platypus.vcf")
+        vcf_file = vcf.read_vcf(vcf_path)
+        vcf_parsed = vcf.parse_vcf(vcf_file, "NR", "NV")
+        annotations_gene = vep.annotate_variants(vcf_parsed, by_gene = True)
+        ## Get the gene IDs and show that the number of gene IDs == the length of the table
+        gene_ids = set([str(line[1]) + "_" + line[3] + "_" + line[9] for line in annotations_gene])
+        self.assertEqual(len(gene_ids), len(annotations_gene))
+
+class test_main(unittest.TestCase):
+    def test_correct_rows(self):
